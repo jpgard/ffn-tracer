43912b0d8c55389dddf4a8298faae147598123fd
jpgard <joshua.patrick.gardner@gmail.com>
2019-11-19 12:57:48 -0800

    added the feature schema for import in other modules

--- fftracer/utils/__init__.py
+++ fftracer/utils/__init__.py
@@ -1,4 +1,15 @@
-
+import tensorflow as tf
 
 # allowed image extensions
 VALID_IMAGE_EXTENSIONS = (".jpg", ".png")
+
+# a dictionary describing the features from mozak data
+FEATURE_SCHEMA = {
+    'shape_x': tf.io.FixedLenFeature(1, tf.int64),
+    'shape_y': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_x': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_y': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_z': tf.io.FixedLenFeature(1, tf.int64),
+    'image_raw': tf.io.VarLenFeature(tf.int64),
+    'image_label': tf.io.VarLenFeature(tf.float32),
+}
