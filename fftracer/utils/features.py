7080579e3db86b5c057f766ebf53604179493e14
jpgard <joshua.patrick.gardner@gmail.com>
2019-11-20 18:23:05 -0800

    now feature functions take lists as input

--- fftracer/utils/features.py
+++ fftracer/utils/features.py
@@ -5,16 +5,14 @@ import tensorflow as tf
 # The following functions can be used to convert a value to a type compatible
 # with tf.Example.
 
-def _bytes_feature(value):
+def _bytes_feature(values: list):
   """Returns a bytes_list from a string / byte."""
-  if isinstance(value, type(tf.constant(0))):
-    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
 
-def _float_feature(value):
+def _float_feature(values: list):
   """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+  return tf.train.Feature(float_list=tf.train.FloatList(value=values))
 
-def _int64_feature(value):
+def _int64_feature(values: list):
   """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
