045659cff0d75e471423697bcf06cb8be5f80687
jpgard <joshua.patrick.gardner@gmail.com>
2019-11-21 08:53:38 -0800

    load_tfrecord_dataset() now returns a map of dataset_id:volume to match ffn code functionality

--- fftracer/training/input.py
+++ fftracer/training/input.py
@@ -3,30 +3,41 @@ Functions for processing model input.
 """
 
 import tensorflow as tf
+import os
 
 def load_tfrecord_dataset(tfrecord_files, feature_schema):
     """
     Load and parse the dataset in tfrecord files.
     :param tfrecord_files: list of files containg tfrecords
     :param feature_schema: dict of {feature_name, tf.io.<feature_type>}
-    :return: the dataset parsed according to the specified feature schema.
+    :return: a dictionary mapping dataset_ids to datasets, which are parsed
+    according to the specified feature schema.
     """
-    raw_image_dataset = tf.data.TFRecordDataset(tfrecord_files)
+    volume_map = dict()
+    for file in tfrecord_files:
+        dataset_id = os.path.splitext(file)[0]
+        raw_image_dataset = tf.data.TFRecordDataset(tfrecord_files)
+        def _parse_image_function(example_proto):
+          # Parse the input tf.Example proto using the dictionary above.
+          return tf.io.parse_single_example(example_proto, feature_schema)
 
-    def _parse_image_function(example_proto):
-      # Parse the input tf.Example proto using the dictionary above.
-      return tf.io.parse_single_example(example_proto, feature_schema)
-
-    parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
-    return parsed_image_dataset
+        parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+        volume_map[dataset_id] = parsed_image_dataset
+    return volume_map
 
 
-def make_batch_from_dataset(dataset, parse_example_fn):
+def get_batch(dataset, parse_example_fn):
     """
     Create a batch of training instances from a tf.Example of a complete neuron.
-    :param dataset:
-    :param parse_example_fn: a function which returns a dict with keys "x", "y", "seed".
-    :return:
+    :param dataset: a dataset containing training examples.
+    :param parse_example_fn: callable which returns a dict with keys "x", "y", "seed".
+    Yields:
+        tuple of:
+          seed array, shape [b, z, y, x, 1]
+          image array, shape [b, z, y, x, 1]
+          label array, shape [b, z, y, x, 1]
+
+        where 'b' is the batch_size.
     """
     for element in dataset:
         parsed_example = parse_example_fn(element)
@@ -48,3 +59,8 @@ def parse_example(example: dict):
     #     skimage.io.imsave("tfrecord_image_raw.jpg", image_raw.numpy())
     #     skimage.io.imsave("tfrecord_image_label.jpg", image_label.numpy())
     return {"x": image_raw, "y": image_label, "seed": seed}
+
+
+def load_patch_coordinates(filename):
+    #TODO(jpgard): mimic logic from ffn's function with equivalent name
+    return
