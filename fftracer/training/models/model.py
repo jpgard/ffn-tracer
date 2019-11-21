55967c43e7f69aa9914b938f964ed9434db176a5
jpgard <joshua.patrick.gardner@gmail.com>
2019-11-20 18:22:42 -0800

    initial commit

---
+++ fftracer/training/models/model.py
@@ -0,0 +1,12 @@
+"""Classes for FFN model definition."""
+
+from ffn.training.models.model import FFNModel
+
+
+class FFNTracerModel(FFNModel):
+    """Base class for FFN tracing models."""
+
+    def __init__(self, deltas, batch_size=None, define_global_step=True, dim=2):
+        self.dim = dim
+        self.deltas = deltas
+        self.batch_size = batch_size
