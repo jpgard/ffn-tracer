ed843311633cc6ca4dbe3db29ed43a452ff40ea6
jpgard <joshua.patrick.gardner@gmail.com>
2019-12-06 13:00:11 -0800

    change _feature() functions to operate on lists of values; add feature schema and image shape functions

--- fftracer/utils/features.py
+++ fftracer/utils/features.py
@@ -2,17 +2,39 @@
 
 import tensorflow as tf
 
+
 # The following functions can be used to convert a value to a type compatible
 # with tf.Example.
 
 def _bytes_feature(values: list):
-  """Returns a bytes_list from a string / byte."""
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
+    """Returns a bytes_list from a string / byte."""
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
+
 
 def _float_feature(values: list):
-  """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+
 
 def _int64_feature(values: list):
-  """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
+
+
+# a dictionary describing the features from mozak data
+FEATURE_SCHEMA = {
+    'shape_x': tf.io.FixedLenFeature(1, tf.int64),
+    'shape_y': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_x': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_y': tf.io.FixedLenFeature(1, tf.int64),
+    'seed_z': tf.io.FixedLenFeature(1, tf.int64),
+    'image_raw': tf.io.VarLenFeature(tf.int64),
+    'image_label': tf.io.VarLenFeature(tf.float32),
+}
+
+
+def get_image_shape(volume_map, volume_name):
+    """Get the input shape as an (x,y) Tensor"""
+    for example_element in volume_map[volume_name]:
+        return tf.concat([example_element["shape_x"], example_element["shape_y"]],
+                         axis=-1)
