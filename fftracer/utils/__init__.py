df034e2aea4252768f55f3023b6d831673f3578b
jpgard <joshua.patrick.gardner@gmail.com>
2019-12-06 12:59:24 -0800

    moved schema to mozak module

--- fftracer/utils/__init__.py
+++ fftracer/utils/__init__.py
@@ -1,15 +1,5 @@
-import tensorflow as tf
-
 # allowed image extensions
 VALID_IMAGE_EXTENSIONS = (".jpg", ".png")
 
-# a dictionary describing the features from mozak data
-FEATURE_SCHEMA = {
-    'shape_x': tf.io.FixedLenFeature(1, tf.int64),
-    'shape_y': tf.io.FixedLenFeature(1, tf.int64),
-    'seed_x': tf.io.FixedLenFeature(1, tf.int64),
-    'seed_y': tf.io.FixedLenFeature(1, tf.int64),
-    'seed_z': tf.io.FixedLenFeature(1, tf.int64),
-    'image_raw': tf.io.VarLenFeature(tf.int64),
-    'image_label': tf.io.VarLenFeature(tf.float32),
-}
+# extension for tfrecord file
+TFRECORD = ".tfrecord"
