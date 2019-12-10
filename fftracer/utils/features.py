b79abaddb3678bfba60e60503f1748869e8643ed
jpgard <joshua.patrick.gardner@gmail.com>
2019-12-10 15:52:05 -0800

    port get_image_mean_and_stddev() to tf 1.x

--- fftracer/utils/features.py
+++ fftracer/utils/features.py
@@ -1,7 +1,8 @@
 """Feature functions for working with Tensorflow data."""
 
 import tensorflow as tf
-from fftracer.training.input import get_dense_array_from_element, get_shape_xy_from_element
+from fftracer.training.input import get_dense_array_from_element, \
+    get_shape_xy_from_element
 
 
 # The following functions can be used to convert a value to a type compatible
@@ -40,18 +41,42 @@ def get_image_shape(volume_map, volume_name):
         return tf.concat([example_element["shape_x"], example_element["shape_y"]],
                          axis=-1)
 
-def get_image_mean_and_stddev(volume_map, volume_name):
+
+def get_image_mean_and_stddev(volume_map, volume_names, name=None,
+                              feature_name='image_raw'):
     """Fetch the mean and stddev pixel values for the raw image.
 
-    Returns two Tensors of shape [1,] containing image mean and stddev, respectively."""
-    # convert volume_name to string representation for indexing into volume_map
-    volume_name = volume_name.numpy()[0].decode("utf-8")
-    # the volume is a dataset of size one; take the first(only) element, fetch its
-    # corresponding image, and reshape to a 2d array
-    element = volume_map[volume_name].__iter__().next()
-    shape_xy = get_shape_xy_from_element(element)
-    volume = get_dense_array_from_element(element, 'image_raw', shape_xy)
-    image_mean = tf.math.reduce_mean(tf.cast(volume, tf.float32))
-    image_stddev = tf.math.reduce_std(tf.cast(volume, tf.float32))
+    :param volume_map: a dictionary mapping volume names to volume objects.  See above
+        for API requirements of the Numpy-like volume objects.
+    :param volume_names: tensor of shape [1] containing names of volumes to load data
+        from.
+    :param name: name for the op.
+    :param feature_name: the name of the feature to grab from the volume.
 
-    return image_mean, image_stddev
+    Returns two Tensors of shape [1,] containing image mean and stddev, respectively."""
+
+    def _compute_feature_mean(volname):
+        """Load from coord and volname, handling 3d or 4d volumes."""
+        volume_data = volume_map[volname.decode('ascii')]
+        volume = tf.sparse.to_dense(volume_data[feature_name])
+        image_mean = tf.math.reduce_mean(tf.cast(volume, tf.float32))
+        return image_mean
+
+    def _compute_feature_stddev(volname):
+        """Load from coord and volname, handling 3d or 4d volumes."""
+        volume_data = volume_map[volname.decode('ascii')]
+        volume = tf.sparse.to_dense(volume_data[feature_name])
+        image_stddev = tf.math.reduce_std(tf.cast(volume, tf.float32))
+        return image_stddev
+
+    with tf.name_scope(name, 'GetImageMeanAndStddev',
+                       [ volume_names]) as scope:
+        # For historical reasons these have extra flat dims.
+        volume_names = tf.squeeze(volume_names, axis=0)
+        image_mean = tf.py_func(
+            _compute_feature_mean, [volume_names], [tf.float32],
+            name='GetImageMean')[0]
+        image_stddev = tf.py_func(
+            _compute_feature_stddev, [volume_names], [tf.float32],
+            name='GetImageStddev')[0]
+        return image_mean, image_stddev
