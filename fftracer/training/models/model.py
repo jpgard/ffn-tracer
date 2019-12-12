f36310955bdccff8920dbc24580d55d05bba6a5a
jpgard <joshua.patrick.gardner@gmail.com>
2019-12-11 18:41:04 -0800

    implementation of FFNTracerModel and related methods

--- fftracer/training/models/model.py
+++ fftracer/training/models/model.py
@@ -4,27 +4,87 @@ from ffn.training.model import FFNModel
 import tensorflow as tf
 
 
+def _predict_object_mask_2d(net, depth=9):
+    """Computes single-object mask prediction for 2d using a 3d conv with 3x3x1 kernel.
+
+    Modified from ffn.training.models.convstack_3d .
+    """
+    conv = tf.contrib.layers.conv3d
+
+    # TODO(jpgard): num_outputs may need to be adjusted (32 is original value when conv
+    #  was set to tf.contrib.layers.conv3d .
+    with tf.contrib.framework.arg_scope([conv], num_outputs=32,
+                                        kernel_size=(3, 3, 1),
+                                        padding='SAME'):
+        net = conv(net, scope='conv0_a')
+        net = conv(net, scope='conv0_b', activation_fn=None)
+
+        for i in range(1, depth):
+            with tf.name_scope('residual%d' % i):
+                in_net = net
+                net = tf.nn.relu(net)
+                net = conv(net, scope='conv%d_a' % i)
+                net = conv(net, scope='conv%d_b' % i, activation_fn=None)
+                net += in_net
+
+    net = tf.nn.relu(net)
+    logits = conv(net, 1, (1, 1, 1), activation_fn=None, scope='conv_lom')
+
+    return logits
+
+
 class FFNTracerModel(FFNModel):
     """Base class for FFN tracing models."""
 
     def __init__(self, deltas, batch_size=None, dim=2,
-                 fov_size=None):
+                 fov_size=None, depth=9):
         """
 
         :param deltas:
         :param batch_size:
-        :param define_global_step:
         :param dim: number of dimensions of model prediction (e.g. 2 = 2D input/output)
         :param fov_size: [x,y,z] fov size.
+        :param depth: number of convolutional layers.
         """
         self.dim = dim
+        super(FFNTracerModel, self).__init__(deltas, batch_size)
+
         self.deltas = deltas
         self.batch_size = batch_size
-        self.input_seed_size = None
+        self.depth = depth
         # The seed is always a placeholder which is fed externally from the
         # training/inference drivers.
-        self.input_seed = tf.compat.v1.placeholder(tf.float32, name='seed')
-        self.input_patches = tf.compat.v1.placeholder(tf.float32, name='patches')
+        self.input_seed = tf.placeholder(tf.float32, name='seed')
+        self.input_patches = tf.placeholder(tf.float32, name='patches')
+        # Set pred_mask_size = input_seed_size = input_image_size = fov_size and also
+        # set input_seed_size = input_image_size = [batch_size, z, y, x, 1]
+        self.set_uniform_io_size(fov_size)
 
-        if fov_size:
-            self.set_uniform_io_size(fov_size)
+
+    def define_tf_graph(self):
+        """Modified for 2D from ffn.training.models.convstack_3d.ConvStack3DFFNModel ."""
+        self.show_center_slice(self.input_seed)
+        if self.input_patches is None:
+            self.input_patches = tf.placeholder(  # [batch_size, x, y, z, num_channels]
+                tf.float32, [1] + list(self.input_image_size[::-1]) + [1],
+                name='patches')
+
+        # TODO(jpgard): concatenation may need to be along axis=3, not axis=4, below.
+        net = tf.concat([self.input_patches, self.input_seed], 4)
+
+        with tf.variable_scope('seed_update', reuse=False):
+            logit_update = _predict_object_mask_2d(net, self.depth)
+        logit_seed = self.update_seed(self.input_seed, logit_update)
+
+        # Make predictions available, both as probabilities and logits.
+        self.logits = logit_seed
+        self.logistic = tf.sigmoid(logit_seed)
+
+        if self.labels is not None:
+            self.set_up_sigmoid_pixelwise_loss(logit_seed)
+            self.set_up_optimizer()
+            self.show_center_slice(logit_seed)
+            self.show_center_slice(self.labels, sigmoid=False)
+            self.add_summaries()
+
+        self.saver = tf.train.Saver(keep_checkpoint_every_n_hours=1)
