2689c287c24efd53c7f9c437b88491bc4effc5cd
jpgard <joshua.patrick.gardner@gmail.com>
2019-12-19 00:03:02 -0500

    use png to avoid compression/rasterization artifacts from jpg

--- fftracer/utils/debugging.py
+++ fftracer/utils/debugging.py
@@ -6,7 +6,7 @@ import numpy as np
 def write_patch_and_label_to_img(patch, label, unique_id, dirname=None):
     if not dirname:
         dirname = os.getcwd()
-    outfile = "patch_and_label_{}.jpg".format(unique_id)
+    outfile = "patch_and_label_{}.png".format(unique_id)
     patch = patch.astype(np.uint8)
     label = label.astype(np.uint8)
     patch_and_label = np.concatenate([patch, label], axis=1)
