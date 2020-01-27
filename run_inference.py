8643ef6c20ca380bbe7e4f156ddccebed70b460f
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-27 13:42:07 -0800

    make a temporary copy of inference hdf5; use InferenceConfig object

--- run_inference.py
+++ run_inference.py
@@ -7,19 +7,21 @@ Forked from ffn/run_inference.py
 usage:
 python run_inference.py \
     --bounding_box 'start { x:0 y:0 z:0 } size { x:7601 y:9429 z:1 }' \
-    --out_dir results/tmp/ \
+    --out_dir results/507727402/ \
     --depth 9 \
     --fov_size 1,${FOV},${FOV} \
     --image "data/test/507727402/507727402_raw.h5:raw" \
     --image_mean 78 \
     --image_stddev 20 \
-    --ckpt_id 3052534 \
-    --move_threshold 0.07
+    --ckpt_id 10000000 \
+    --move_threshold 0.12
 
 """
 
 import logging
 import os
+import shutil
+import tempfile
 import time
 
 from google.protobuf import text_format
@@ -39,7 +41,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('bounding_box', None,
                     'BoundingBox proto in text format defining the area '
                     'to be segmented.')
-flags.DEFINE_string("out_dir", None, "directory to save to")
+flags.DEFINE_string("out_dir", None, "relative path of directory to save to")
 flags.DEFINE_integer("depth", 9, "number of residual blocks in model")
 flags.DEFINE_list('fov_size', [1, 49, 49], '[z, y, x] size of training fov')
 flags.DEFINE_list('deltas', [0, 8, 8], '[z, y, x] size of training fov')
@@ -76,6 +78,22 @@ flags.DEFINE_string('seed_policy', 'ManualSeedPolicy',
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
+def copy_file_to_tempdir(fp, dirname):
+    """Copy the file at fp to dirname, keeping the same base name.
+    Returns the path to the new file, as string.
+    """
+    fp, dset_name = fp.split(":")
+    assert os.path.isdir(dirname)
+    assert os.path.exists(fp)
+    fsize = os.path.getsize(fp)
+    if fsize > 1e8:
+        logging.warning("Copying large file; size in bytes: {}".format(fsize))
+    filename = os.path.basename(fp)
+    shutil.copy(fp, dirname)
+    dest_fp = os.path.join(dirname, filename)
+    return dest_fp + ":" + dset_name
+
+
 def main(unused_argv):
 
     move_threshold = FLAGS.move_threshold
@@ -88,58 +106,71 @@ def main(unused_argv):
                 depth=FLAGS.depth,
                 fov=max(fov_size.values())
                 )
-    segmentation_output_dir = FLAGS.out_dir + model_uid + "mt" + str(move_threshold)
+    segmentation_output_dir = os.path.join(
+        os.getcwd(),
+        FLAGS.out_dir + model_uid + "mt" + str(move_threshold)
+    )
     model_checkpoint_path = "{train_dir}/{model_uid}/model.ckpt-{ckpt_id}"\
         .format(train_dir=FLAGS.train_dir,
                 model_uid=model_uid,
                 ckpt_id=FLAGS.ckpt_id)
+    if not gfile.Exists(segmentation_output_dir):
+        gfile.MakeDirs(segmentation_output_dir)
+    else:
+        logging.warning(
+            "segmentation_output_dir {} already exists; this may cause inference to "
+            "terminate without running.".format(segmentation_output_dir))
 
-    inference_config = InferenceConfig(
-        image=FLAGS.image,
-        fov_size=fov_size,
-        deltas=deltas,
-        depth=FLAGS.depth,
-        image_mean=FLAGS.image_mean,
-        image_stddev=FLAGS.image_stddev,
-        model_checkpoint_path=model_checkpoint_path,
-        model_name=FLAGS.model_name,
-        segmentation_output_dir=segmentation_output_dir,
-        move_threshold=move_threshold,
-        min_segment_size=FLAGS.min_segment_size,
-        segment_threshold=FLAGS.segment_threshold,
-        min_boundary_dist=min_boundary_dist,
-        seed_policy=FLAGS.seed_policy
-    )
-    config = inference_config.to_string()
-    logging.info(config)
-    req = inference_pb2.InferenceRequest()
-    _ = text_format.Parse(config, req)
+    with tempfile.TemporaryDirectory(dir=segmentation_output_dir) as tmpdir:
 
-    if not gfile.Exists(inference_config.segmentation_output_dir):
-        gfile.MakeDirs(inference_config.segmentation_output_dir)
+        # Create a temporary local copy of the HDF5 image, because simulataneous access
+        # to HDF5 files is not allowed (only recommended for small files).
 
-    bbox = bounding_box_pb2.BoundingBox()
-    text_format.Parse(FLAGS.bounding_box, bbox)
+        temp_image = copy_file_to_tempdir(FLAGS.image, tmpdir)
 
-    runner = inference.Runner()
-    runner.start(req)
+        inference_config = InferenceConfig(
+            image=temp_image,
+            fov_size=fov_size,
+            deltas=deltas,
+            depth=FLAGS.depth,
+            image_mean=FLAGS.image_mean,
+            image_stddev=FLAGS.image_stddev,
+            model_checkpoint_path=model_checkpoint_path,
+            model_name=FLAGS.model_name,
+            segmentation_output_dir=segmentation_output_dir,
+            move_threshold=move_threshold,
+            min_segment_size=FLAGS.min_segment_size,
+            segment_threshold=FLAGS.segment_threshold,
+            min_boundary_dist=min_boundary_dist,
+            seed_policy=FLAGS.seed_policy
+        )
+        config = inference_config.to_string()
+        logging.info(config)
+        req = inference_pb2.InferenceRequest()
+        _ = text_format.Parse(config, req)
 
-    start_zyx = (bbox.start.z, bbox.start.y, bbox.start.x)
-    size_zyx = (bbox.size.z, bbox.size.y, bbox.size.x)
-    logging.info("Running; start at {} size {}.".format(start_zyx, size_zyx))
 
-    # Segmentation is attempted from all valid starting points provided by the seed
-    # policy by calling runner.canvas.segment_all().
-    runner.run(start_zyx,
-               size_zyx)
-    logging.info("Finished running.")
+        bbox = bounding_box_pb2.BoundingBox()
+        text_format.Parse(FLAGS.bounding_box, bbox)
+        runner = inference.Runner()
+        runner.start(req)
 
-    counter_path = os.path.join(inference_config.segmentation_output_dir, 'counters.txt')
-    if not gfile.Exists(counter_path):
-        runner.counters.dump(counter_path)
+        start_zyx = (bbox.start.z, bbox.start.y, bbox.start.x)
+        size_zyx = (bbox.size.z, bbox.size.y, bbox.size.x)
+        logging.info("Running; start at {} size {}.".format(start_zyx, size_zyx))
 
-    runner.stop_executor()
-    del runner
+        # Segmentation is attempted from all valid starting points provided by the seed
+        # policy by calling runner.canvas.segment_all().
+        runner.run(start_zyx,
+                   size_zyx)
+        logging.info("Finished running.")
+
+        counter_path = os.path.join(inference_config.segmentation_output_dir, 'counters.txt')
+        if not gfile.Exists(counter_path):
+            runner.counters.dump(counter_path)
+
+        runner.stop_executor()
+        del runner
 
 
 if __name__ == '__main__':
