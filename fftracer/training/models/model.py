d665fad88b6a52f5af30a255b8bac8a7ac524cf2
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-26 16:59:39 -0800

    stub of generalized dice loss; set default model dim to 3 (this is always the value used)

--- fftracer/training/models/model.py
+++ fftracer/training/models/model.py
@@ -37,7 +37,7 @@ def _predict_object_mask_2d(net, depth=9):
 class FFNTracerModel(FFNModel):
     """Base class for FFN tracing models."""
 
-    def __init__(self, deltas, batch_size=None, dim=2,
+    def __init__(self, deltas, batch_size=None, dim=3,
                  fov_size=None, depth=9, loss_name="sigmoid_pixelwise", alpha=1e-6):
         """
 
@@ -205,6 +205,14 @@ class FFNTracerModel(FFNModel):
 
         self.alpha_weight_losses(batch_ce_loss, batch_boundary_loss)
 
+    def set_up_generalized_dice_loss(self, logits):
+        """Apply the Generalized Dice Loss from https://arxiv.org/pdf/1707.03237.pdf"""
+        from niftynet.layer.loss_segmentation import generalised_dice_loss as gdl
+        dice_loss = gdl(logits, self.labels)
+        dice_loss_batch = tf.reduce_mean(dice_loss)
+        import ipdb;ipdb.set_trace()
+
+
 
 
     def set_up_loss(self, logit_seed):
@@ -219,6 +227,8 @@ class FFNTracerModel(FFNModel):
             self.set_up_ms_ssim_loss(logit_seed)
         elif self.loss_name == "boundary":
             self.set_up_boundary_loss(logit_seed)
+        elif self.loss_name == "gdl":
+            self.set_up_generalized_dice_loss(logit_seed)
         else:
             raise NotImplementedError
 
