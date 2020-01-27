d4807fa8b536dc25db63be3a632aaafe02622509
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-27 13:15:55 -0800

    adding; forgot to add previously

---
+++ fftracer/utils/flags.py
@@ -0,0 +1,19 @@
+"""
+Utilities for working with abseil flags.
+"""
+import json
+
+def uid_from_flags(flags):
+    """Construct a uique string identifier for an experiment."""
+    model_args = json.loads(flags.model_args)
+    uid = "bs{batch_size}lr{learning_rate}opt{optimizer}".format(
+        batch_size=flags.batch_size,
+        learning_rate=flags.learning_rate,
+        optimizer=flags.optimizer
+    )
+    # model_args are handled separately; these require some extra logic to parse
+    uid += "fov{}".format("".join([str(x) for x in model_args.pop("fov_size")]))
+    uid += "loss{}".format(model_args.pop("loss_name"))
+    uid += "deltas{}".format("".join([str(x) for x in model_args.pop("deltas")]))
+    uid += "".join([str(k) + str(v) for k,v in model_args.items()])
+    return uid
