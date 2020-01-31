346268072f301d2470d7ba95a92a0726a55476a1
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-30 18:21:58 -0800

    change padding in max_pooling layers to SAME; dynamically infer size of downsampled_num dimension in the intermediary attention matrixes f(x), g(x) by using the known batch_size and num_channels; style updates

--- fftracer/training/self_attention/non_local.py
+++ fftracer/training/self_attention/non_local.py
@@ -15,7 +15,7 @@
 
 import tensorflow as tf
 import numpy as np
-import ops
+from . import ops
 
 def conv1x1(input_, output_dim,
             init=tf.contrib.layers.xavier_initializer(), name='conv1x1'):
@@ -40,25 +40,30 @@ def sn_conv1x1(input_, output_dim, update_collection,
     w = tf.get_variable(
         'w', [k_h, k_w, input_.get_shape()[-1], output_dim],
         initializer=init)
-    w_bar = ops.spectral_normed_weight(w, num_iters=1, update_collection=update_collection)
+    w_bar = ops.spectral_normed_weight(w, num_iters=1,
+                                       update_collection=update_collection)
 
     conv = tf.nn.conv2d(input_, w_bar, strides=[1, d_h, d_w, 1], padding='SAME')
     return conv
 
-def sn_non_local_block_sim(x, update_collection, name, init=tf.contrib.layers.xavier_initializer()):
+def sn_non_local_block_sim(x, update_collection, name,
+                           init=tf.contrib.layers.xavier_initializer()):
   with tf.variable_scope(name):
     batch_size, h, w, num_channels = x.get_shape().as_list()
     location_num = h * w
-    downsampled_num = location_num // 4
+    # downsampled_num = location_num // 4
+    downsampled_num = -1  # this sets the matrix dimensions automatically using
+    # batch_size and num_channels
 
-    # theta path
+    # theta path (JG: 'theta' is the result of g(x) in Fig. 2 of SAGAN paper)
     theta = sn_conv1x1(x, num_channels // 8, update_collection, init, 'sn_conv_theta')
     theta = tf.reshape(
         theta, [batch_size, location_num, num_channels // 8])
 
-    # phi path
+    # phi path (JG: 'phi' is the result of f(x) in Fig. 2 of SAGAN paper)
     phi = sn_conv1x1(x, num_channels // 8, update_collection, init, 'sn_conv_phi')
-    phi = tf.layers.max_pooling2d(inputs=phi, pool_size=[2, 2], strides=2)
+    phi = tf.layers.max_pooling2d(inputs=phi, pool_size=[2, 2], strides=2, padding='SAME')
+    # JG: for FOV size of [49, 49], phi will have shape [4, 25, 25, 4] at this step.
     phi = tf.reshape(
         phi, [batch_size, downsampled_num, num_channels // 8])
 
@@ -67,9 +72,9 @@ def sn_non_local_block_sim(x, update_collection, name, init=tf.contrib.layers.xa
     attn = tf.nn.softmax(attn)
     print(tf.reduce_sum(attn, axis=-1))
 
-    # g path
+    # g path (JG: Confusingly, 'g' is actually the result of h(x) in Fig. 2)
     g = sn_conv1x1(x, num_channels // 2, update_collection, init, 'sn_conv_g')
-    g = tf.layers.max_pooling2d(inputs=g, pool_size=[2, 2], strides=2)
+    g = tf.layers.max_pooling2d(inputs=g, pool_size=[2, 2], strides=2, padding='SAME')
     g = tf.reshape(
       g, [batch_size, downsampled_num, num_channels // 2])
 
