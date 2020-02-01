cda775485bc3a3f292b0f2e4bdaf4783aa38a87b
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-31 18:06:44 -0800

    initial implementation of DCGAN class

--- fftracer/training/models/dcgan.py
+++ fftracer/training/models/dcgan.py
@@ -1,27 +1,46 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+
+from fftracer.utils.tensor_ops import drop_axis
 
 
-def make_discriminator_model(input_shape):
-    """
-    Make a DCGAN-style discriminator.
+class DCGAN:
+    def __init__(self, input_shape, dim=2):
+        assert dim in (2, 3)
+        assert len(input_shape) == dim + 1, "input should have shape (dim + 1)"
+        self.dim = dim
+        self.input_shape = input_shape
 
-    The discriminator takes a tensor of shape [batch_size, z, y, x, num_channels] and
-    outputs a single real value indicating whether this is a batch of real or fake
-    samples.
-    :return:
-    """
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                            input_shape=[28, 28, 1]))
-    model.add(layers.LeakyReLU(alpha=0.2))
-    model.add(layers.Dropout(0.3))
+    def predict_discriminator_2d(self, net):
+        """
+        Use a DCGAN-style discriminator to predict whether a batch is real or fake.
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU(alpha=0.2))
-    model.add(layers.Dropout(0.3))
+        The discriminator takes a tensor of shape [batch_size, y, x, num_channels] and
+        outputs a single real value indicating whether this is a batch of real or fake
+        samples.
+        """
+        if self.dim == 2:
+            net = drop_axis(net, axis=1, name="drop_z_2d_discriminator")
+        input_batch_shape = net.get_shape().as_list()[1:]
+        assert input_batch_shape == self.input_shape, \
+            "discriminator input has shape {}, does not match expected shape {}".format(
+                 input_batch_shape, self.input_shape
+            )
+        with tf.variable_scope('discriminator', reuse=False):
+            net = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='SAME',
+                                         input_shape=self.input_shape)(net)
+            net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
+            net = tf.keras.layers.Dropout(0.3)(net)
+            net = tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')(net)
+            net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
+            net = tf.keras.layers.Dropout(0.3)(net)
+            net = tf.keras.layers.Flatten()(net)
+            batch_pred = tf.keras.layers.Dense(1)(net)
+            # TODO(jpgard): should we apply sigmoid here, since we want a value in
+            #  range (0,1) ?
+            return batch_pred
 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
-
-    return model
+    def predict_discriminator(self, batch):
+        if self.dim == 2:
+            return self.predict_discriminator_2d(batch)
+        elif self.dim == 3:
+            raise NotImplementedError
