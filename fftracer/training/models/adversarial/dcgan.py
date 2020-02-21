2663e130bea8711fd0198d55e882636c6b470677
jpgard <joshua.patrick.gardner@gmail.com>
2020-02-21 09:36:53 -0800

    break dcgan prediction into separate function; it is simpler to have separate logic for the different models

--- fftracer/training/models/adversarial/dcgan.py
+++ fftracer/training/models/adversarial/dcgan.py
@@ -19,19 +19,9 @@ class DCGAN(Discriminator):
         else:
             raise NotImplementedError
 
-    def predict_discriminator_2d(self, net):
-        """
-        Use a DCGAN-style discriminator to predict whether a batch is real or fake.
-
-        The discriminator takes a tensor of shape [batch_size, y, x, num_channels] and
-        outputs a single real value indicating whether this is a batch of real or fake
-        samples.
-        """
+    def predict_basic_dcgan_2d(self, net):
         conv = self.get_conv()
-        assert self.dim == 2
-        net = drop_axis(net, axis=1, name="drop_z_2d_discriminator")
-        input_batch_shape = net.get_shape().as_list()[1:]
-        self.check_valid_input_shape(input_batch_shape)
+
         with tf.variable_scope(self.d_scope_name, reuse=False):
             net = conv(filters=64, input_shape=self.input_shape)(net)
             net = tf.keras.layers.Dropout(0.3)(net)
@@ -40,3 +30,24 @@ class DCGAN(Discriminator):
             net = tf.keras.layers.Flatten()(net)
             batch_pred = tf.keras.layers.Dense(1)(net)
             return batch_pred
+
+    def predict_discriminator_2d(self, net):
+        """
+        Use a DCGAN-style discriminator to predict whether a batch is real or fake.
+
+        The discriminator takes a tensor of shape [batch_size, y, x, num_channels] and
+        outputs a single real value indicating whether this is a batch of real or fake
+        samples.
+        """
+        # check the inputs and drop the z-axis
+        assert self.dim == 2
+        net = drop_axis(net, axis=1, name="drop_z_2d_discriminator")
+        input_batch_shape = net.get_shape().as_list()[1:]
+        self.check_valid_input_shape(input_batch_shape)
+
+        if not self.spectral_normalization:
+            return self.predict_basic_dcgan_2d(net)
+        else:
+            # use spectral normalization
+            raise
+
