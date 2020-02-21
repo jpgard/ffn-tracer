32eb12ad2d824c53ea78aed16ffe44dac42565ef
jpgard <joshua.patrick.gardner@gmail.com>
2020-02-21 09:27:19 -0800

    add a spectral normalization attribute to Discriminator class; use a conv variable to reduce duplicated code in dcgan.py

--- fftracer/training/models/adversarial/dcgan.py
+++ fftracer/training/models/adversarial/dcgan.py
@@ -2,6 +2,8 @@
 Class to emulate the DCGAN discriminator network.
 """
 
+from functools import partial
+
 import tensorflow as tf
 
 from fftracer.utils.tensor_ops import drop_axis
@@ -9,6 +11,13 @@ from fftracer.training.models.adversarial import Discriminator
 
 
 class DCGAN(Discriminator):
+    def get_conv(self):
+        """Get the convolution operation for this model."""
+        if not self.spectral_normalization:
+            return partial(tf.keras.layers.Conv2D, kernel_size=(5, 5), strides=(2, 2),
+                           padding='SAME', activation=tf.nn.leaky_relu)
+        else:
+            raise NotImplementedError
 
     def predict_discriminator_2d(self, net):
         """
@@ -18,19 +27,16 @@ class DCGAN(Discriminator):
         outputs a single real value indicating whether this is a batch of real or fake
         samples.
         """
+        conv = self.get_conv()
         assert self.dim == 2
         net = drop_axis(net, axis=1, name="drop_z_2d_discriminator")
         input_batch_shape = net.get_shape().as_list()[1:]
         self.check_valid_input_shape(input_batch_shape)
         with tf.variable_scope(self.d_scope_name, reuse=False):
-            net = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='SAME',
-                                         input_shape=self.input_shape)(net)
-            net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
+            net = conv(filters=64, input_shape=self.input_shape)(net)
             net = tf.keras.layers.Dropout(0.3)(net)
-            net = tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')(net)
-            net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
+            net = conv(filters=128)(net)
             net = tf.keras.layers.Dropout(0.3)(net)
             net = tf.keras.layers.Flatten()(net)
             batch_pred = tf.keras.layers.Dense(1)(net)
             return batch_pred
-
