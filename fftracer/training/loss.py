use compute_alpha_batch; minor bug fixes and shape checking to alpha computation

fftracer/training/loss.py
@@ -136,6 +136,9 @@ def compute_pixel_loss_batch(Pi: np.ndarray, alpha: np.ndarray, D: np.ndarray):
         "Pi and D must match in final two dimensions; got shapes {} {}".format(
             Pi.shape[-2:], D.shape[-2:]
         )
+    assert Pi.shape[0] == alpha.shape[0], \
+    "batch_dim of Pi and alpha do not match; batch_dim " \
+    "shapes are {} {}".format(Pi.shape[0], alpha.shape[0])
     image_pixel_loss = list()
     for i in np.arange(Pi.shape[0]):
         Pi_i = Pi[i, ...]
@@ -148,7 +151,7 @@ def compute_pixel_loss_batch(Pi: np.ndarray, alpha: np.ndarray, D: np.ndarray):
 def compute_alpha(y: np.ndarray, y_hat: np.ndarray):
     A = y.astype(np.float64).sum()
     B = y_hat.astype(np.float64).sum()
-    alpha = tf.math.minimum(B / (2.0 * A), 1.0)
+    alpha = min(B / (2.0 * A), 1.0)
     return alpha
 
 

fftracer/training/models/model.py
@@ -13,7 +13,7 @@ from scipy.special import expit
 from ffn.training.model import FFNModel
 from fftracer.training.self_attention.non_local import sn_non_local_block_sim
 from fftracer.training.loss import make_distance_matrix, compute_ot_loss_matrix_batch,\
-    compute_pixel_loss_batch, compute_alpha
+    compute_pixel_loss_batch, compute_alpha_batch
 from fftracer.training.models.adversarial.dcgan import DCGAN
 from fftracer.training.models.adversarial.patchgan import PatchGAN
 import tensorflow as tf
@@ -394,7 +394,7 @@ class FFNTracerModel(FFNModel):
         _compute_pixel_loss_batch = partial(compute_pixel_loss_batch, D=self.D)
 
         # compute the alpha
-        alpha = tf.py_func(compute_alpha, [y_true, y_hat_probs], tf.float64,
+        alpha = tf.py_func(compute_alpha_batch, [y_true, y_hat_probs], tf.float64,
                            name='ComputeAlpha')
 
         # TODO(jpgard): can we use tf.map_fn here instead?