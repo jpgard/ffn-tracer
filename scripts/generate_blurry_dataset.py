f9c4e08969e38749768031f21a8b22f154768677
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-05 16:55:43 -0800

    add img_dir, update imports, pass paramteers needed for coordinates generation

--- scripts/generate_blurry_dataset.py
+++ scripts/generate_blurry_dataset.py
@@ -9,12 +9,13 @@ python scripts/generate_blurry_dataset.py \
     --dataset_ids 319215569 327671477 476912429 \
     --gs_dir data/gold_standard \
     --out_dir ./data/blurry \
+    --img_dir data/img \
     --train_data_sampling proportional_by_dataset
 """
 
 import argparse
 from fftracer.datasets.blurry import BlurryDataset2D
-# from fftracer.datasets import offset_dict_to_csv
+from fftracer.datasets import offset_dict_to_csv
 import os.path as osp
 
 from PIL import Image
@@ -23,17 +24,16 @@ from PIL import Image
 Image.MAX_IMAGE_PIXELS = None
 
 
-def main(dataset_ids, out_dir, gs_dir, train_data_sampling):
+def main(dataset_ids, out_dir, gs_dir, train_data_sampling, img_dir):
     for dataset_id in dataset_ids:
         dset = BlurryDataset2D(dataset_id)
-        dset.load_and_generate_data(gs_dir)
+        dset.load_and_generate_data(gs_dir, img_dir)
         # write the synthetic data
         dset.write_tfrecord(out_dir)
         # write some synthetic coordinates
         dset.generate_and_write_training_coordinates(
-            out_dir,
-            # num_training_coords # TODO(jpgard): implement
-        )
+            out_dir=out_dir, method=train_data_sampling, coord_margin=200,
+            coord_sampling_prob=0.25)
 
 
 if __name__ == "__main__":
@@ -41,6 +41,9 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_ids", help="dataset id", nargs="+", required=True)
     parser.add_argument("--out_dir", help="directory to save to", required=True)
     parser.add_argument("--gs_dir", help="directory containing gold-standard traces",
+                        required=True)
+    parser.add_argument("--img_dir", help="directory containing raw input images; "
+                                          "these are used to get the shape of the image",
                         required=True)
     parser.add_argument("--train_data_sampling",
                         help="method to use for sampling training data; currently "
